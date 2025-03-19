41a2a27f3297641a27ee624eb17fe61df86bca4c
Charlettine.L <lucascharlettine91@gmail.com>
2025-03-19 15:43:24 +0100

    Update

--- classe_et_methode.py
+++ classe_et_methode.py
@@ -1,9 +1,27 @@
 """Projet info : 
+1. Périmètre du sujet : 
 
+1.1. Crééer un labyrinte de taille n*p : 
 
+1.2. Nos différents classes : 
+
+- un labyrinthe qui contient : la position des phéromones, la quantité de phéromone en une case (atténuation à chauqe tour)
+
+- Un classe fourmi (méthode : capte mur sur sa case, ordeur cases autour, nourriture autour si pas de mur, déposer deux types de phéromones sur sa case, se déplacer d'une cas,
+se souvenir de la case qu'elle vient de visiter, comportement (savoir quand switch de mode))
+
+- Une sous classe de fourmi qui est son comportement 
+    méthode : 
+    -> mode exploration 
+    -> mode suivi de chemin
+    -> mode retour de cul de sac 
+
+- une classe phéromone ? 
+    
 
 
 
 
 """
 print ('gooo')
+
