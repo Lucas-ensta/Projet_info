84ffbf5f7e41de92a2966199ffb499fb1f3c75f4
Charlettine.L <lucascharlettine91@gmail.com>
2025-05-28 15:38:32 +0200

    nouvelle classe pour renvoyer le chemin a suivre le plus rapide (meme focntionnement que les autre comportements)

--- comportement.py
+++ comportement.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
-
 import random
+from collections import deque
+from labyrinthe import Labyrinthe
+from simulation import Simulation
+import matplotlib.pyplot as plt 
+from fourmi import *
 
-from labyrinthe import *
 
 class Comportement(ABC):
     """
@@ -11,8 +14,8 @@ class Comportement(ABC):
     @abstractmethod
     def choisir_direction(self,fourmi,labyrinthe):
         pass
-    
-    @abstractmethod 
+
+    @abstractmethod
     def choisir_pheromone(self, fourmi, labyrinthe):
         pass
 
@@ -21,72 +24,57 @@ class Exploration(Comportement):
     Classe fille de comportement modélisant le comportement d'exploration
     """
 
-    def choisir_direction(self,fourmi,labyrinthe):
+    def choisir_direction(self, fourmi, labyrinthe):
         """
         Méthode permettant à la fourmi de choisir le chemin avec le moins de phéromones afin d'explorer le labyrinthe
         :param fourmi : La fourmi
         :param labyrinthe: Le labyrinthe
         :return: La direction choisie sous forme de str
         """
-        
-        nourriture = fourmi.percevoir_nourriture(labyrinthe) #aller à la nourriture si il y en a 
-        for x in nourriture.keys() :
-            if nourriture[x] is True :
-                
+        nourriture = fourmi.percevoir_nourriture(labyrinthe)  # aller à la nourriture si il y en a
+        for x in nourriture.keys():
+            if nourriture[x] is True:
                 return x
-
         chemins = fourmi.chemins_possible(labyrinthe)  # liste des directions possible sous forme de str
-        
-        
         chemin_choisi = []
-
-        if len(chemins) == 1: # Attention cul-de-sac, passer en mode Retour et déposer une phéromnes repulsive  
+        if len(chemins) == 1:  # Attention cul-de-sac, passer en mode Retour et déposer une phéromnes repulsive
             print("cul de sac")
             return chemins[0]
-
-
         else:
             for c in chemins:
                 i, j = fourmi.conversion_str_int(c)  # stock l'indice de la case donné par la direction c
                 pheromone = labyrinthe.etat_case[i][j].pheromones  # c'est un dictionnaire avec 2 clés
-                 
-                val_case =  pheromone["attractif"] - pheromone["repulsif"]  # on regarde l'attractivité de la case
-
+                val_case = pheromone["attractif"] - pheromone["repulsif"]  # on regarde l'attractivité de la case
                 if val_case >= 0:  # on ne garde que les cases attractives
                     chemin_choisi.append((c, val_case))
-
             if not chemin_choisi:  # aucun chemin n'est attractif ou neutre, on créer la meme liste que précédemment sans filtrage
                 for c in chemins:
                     i, j = fourmi.conversion_str_int(c)
                     pheromone = labyrinthe.etat_case[i][j].pheromones
                     chemin_choisi.append((c, pheromone["repulsif"]))
+        # On récupère la valeur de case minimale
+        min_val = min(chemin_choisi, key=lambda x: x[1])[1]
+        # On garde toutes les directions ayant cette valeur
+        meilleurs_chemins = [c for c, val in chemin_choisi if val == min_val]
+        # On en choisit un au hasard
+        chemin_choisi_final = random.choice(meilleurs_chemins)
+        return chemin_choisi_final
 
-            # On récupère la valeur de case minimale 
-            min_val = min(chemin_choisi, key=lambda x: x[1])[1]
-            # On garde toutes les directions ayant cette valeur
-            meilleurs_chemins = [c for c, val in chemin_choisi if val == min_val]
-            # On en choisit un au hasard
-            chemin_choisi_final = random.choice(meilleurs_chemins)
-            
-            
-        return  chemin_choisi_final
-    
     def choisir_pheromone(self, fourmi, labyrinthe):
         """
         Methode permettant à la fourmi de déposer au non des phéromones. Dans le cas Exploration, la fourmi dépose des pheromones attractive +
-        ssi elle est sur une case adjacente à la nourriture 
+        ssi elle est sur une case adjacente à la nourriture
         :param fourmi: La fourmi
         :param labyrinthe: Le labyrinthe
-        :return: le type de pherome à déposer 
+        :return: le type de pherome à déposer
         """
         chemins = fourmi.chemins_possible(labyrinthe)
-        if len(chemins) == 1 : # C'est un cul de sac 
+        if len(chemins) == 1 : # C'est un cul de sac
             return "repulsif"
         nourriture = fourmi.percevoir_nourriture(labyrinthe)
-        for x in nourriture.keys() :
-            if nourriture[x] is not None :
-                return "attractif" 
-
+        for x in nourriture.keys():
+            if nourriture[x] is True :
+                return "attractif"
 
 
 class Suivi(Comportement):
@@ -94,19 +82,17 @@ class Suivi(Comportement):
     Classe abstraite permettant de modéliser le comportement de suivi de phéromones attractives
     """
 
-    def choisir_direction(self,fourmi,labyrinthe):
+    def choisir_direction(self, fourmi, labyrinthe):
         """
         La fourmi suit le chemin avec les phéromones les plus attractives pour arriver plus vite à la nourriture
         :param fourmi : La fourmi
         :param labyrinthe: Le labyrinthe
         :return: la direction choisie sous forme de str
         """
-
-        nourriture = fourmi.percevoir_nourriture(labyrinthe) #aller à la nourriture si il y en a 
-        for x in nourriture.keys() :
-            if nourriture[x] is True :
+        nourriture = fourmi.percevoir_nourriture(labyrinthe)  # aller à la nourriture si il y en a
+        for x in nourriture.keys():
+            if nourriture[x] is True:
                 return x
-            
         chemins = fourmi.chemins_possible(labyrinthe)  # liste des directions possible sous forme de str
         chemin_choisi = []
         if len(chemins) == 1:  # il n'ya qu'une direction possible
@@ -115,82 +101,161 @@ class Suivi(Comportement):
             for c in chemins:
                 i, j = fourmi.conversion_str_int(c)
                 pheromone = labyrinthe.etat_case[i][j].pheromones  # c'est un dictionnaire avec 2 clés
-                
-                val_case =  pheromone["attractif"] - pheromone["repulsif"]  # on regarde l'attractivité de la case
+
+                val_case = pheromone["attractif"] - pheromone["repulsif"]  # on regarde l'attractivité de la case
                 chemin_choisi.append((c, val_case))
 
-            # Choix aléatoire parmi les meilleurs chemins (évite l'oscillation)
-            max_val = max(chemin_choisi, key=lambda x: x[1])[1]
-            meilleurs_chemins = [c for c, val in chemin_choisi if val == max_val]
-            choix = random.choice(meilleurs_chemins)
+        # Choix aléatoire parmi les meilleurs chemins (évite l'oscillation)
+        max_val = max(chemin_choisi, key=lambda x: x[1])[1]
+        meilleurs_chemins = [c for c, val in chemin_choisi if val == max_val]
+        choix = random.choice(meilleurs_chemins)
         return choix
 
-
     def choisir_pheromone(self, fourmi, labyrinthe):
-            """
-            Methode permettant à la fourmi de déposer au non des phéromones. Dans le cas Suivi, la fourmi dépose des pheromones attractive +
-            ssi elle est sur une case adjacente à la une phéromone attractive 
-            :param fourmi: La fourmi
-            :param labyrinthe: Le labyrinthe
-            :return: le type de pherome à déposer 
-            """
-            chemins = fourmi.chemins_possible(labyrinthe)
-            if len(chemins) == 1 : # C'est un cul de sac 
-                return "repulsif"
-            pheromone = fourmi.percevoir_pheromones(labyrinthe)
-            for x in pheromone.keys() :
-                if pheromone[x] > 0 :
-                    return "attractif" 
+        """
+        Methode permettant à la fourmi de déposer au non des phéromones. Dans le cas Suivi, la fourmi dépose des pheromones attractive +
+        ssi elle est sur une case adjacente à la une phéromone attractive
+        :param fourmi: La fourmi
+        :param labyrinthe: Le labyrinthe
+        :return: le type de pherome à déposer
+        """
+        chemins = fourmi.chemins_possible(labyrinthe)
+        if len(chemins) == 1:  # C'est un cul de sac
+            return "repulsif"
+        pheromone = fourmi.percevoir_pheromone(labyrinthe)
+        for x in pheromone.keys():
+            if pheromone[x] > 0:
+                return "attractif"
+
 
 class Retour(Comportement):
+    """
+    Classe permettant à la fourmi de revenir sur ses pas lorsqu'elle se retrouve dans un cul de sac
+    """
 
     def choisir_direction(self, fourmi, labyrinthe):
         """
-        Permet à la fourmi de sortir d'un cul de sac 
+        Permet à la fourmi de sortir d'un cul de sac
         :param fourmi : La fourmi
         :param labyrinthe: Le labyrinthe
         :return: la direction choisie sous forme de str
         """
-        chemins = fourmi.chemins_possible(labyrinthe) 
+        chemins = fourmi.chemins_possible(labyrinthe)
+        
         i, j = fourmi.memoire[0]
-        for c in chemins : 
+        for c in chemins:
             n, m = fourmi.conversion_str_int(c)
-            if (i, j) == (n, m) :
+            if (i, j) == (n, m) and len(chemins) > 1:
                 chemins.remove(c)
-        
-        if len(chemins) == 1 : #on est toujours dans le cul de sac, on reste en mode retour 
+        if len(chemins) == 1:  # on est toujours dans le cul de sac, on reste en mode retour
             return chemins[0]
+            
+        elif len(chemins) > 1:  # On est sorti du cul de sac, repasse en mode exploration
+            chem = chemins[random.randint(0, len(chemins) - 1)]
+            return chem
+            
         
-        elif len(chemins) > 1 : #On est sorti du cul de sac, repasse en mode exploration 
-            return chemins[random.randint(0, len(chemins)-1)]
 
     def choisir_pheromone(self, fourmi, labyrinthe):
-            """
-            Methode permettant à la fourmi de déposer au non des phéromones. Dans le cas Retour, la fourmi dépose des pheromones repulsive 
-            toutjours
-            :param fourmi: La fourmi
-            :param labyrinthe: Le labyrinthe
-            :return: le type de pherome à déposer 
-            """
-
-            return "repulsif"
-
-
-if __name__ == "__main__":
-    lab = Labyrinthe(10, 10)
-    lab.creation_labyrinthe()
-    lab.afficher_labyrinthe()
-    sim = Simulation(lab, 4, 2)
-
-    for _ in range (12):
-        sim.etape()
+        """
+        Methode permettant à la fourmi de déposer au non des phéromones. Dans le cas Retour, la fourmi dépose des pheromones repulsive
+        toutjours
+        :param fourmi: La fourmi
+        :param labyrinthe: Le labyrinthe
+        :return: le type de pherome à déposer
+        """
+        return "repulsif"
     
-    lab.afficher_fourmilliere(sim.position_fourmiliere)
-    lab.afficher_nourriture(sim.puit_nourriture)
-    print(sim.fourmis)
-    lab.afficher_fourmis(sim.fourmis)
+
+
+
+class Super(Comportement):
+    """
+    Comportement utilisant un algorithme BFS ( Breadth-First Search) pour trouver le chemin le plus court vers la nourriture.
+    """
+
+    def __init__(self):
+        # Mémoire du chemin calculé (liste de directions)
+        self.chemin_a_suivre = []
+
+    def choisir_direction(self, fourmi, labyrinthe, sim):
+        # Si on a un chemin à suivre déjà calculé et non vide, on prend la prochaine étape
+        if self.chemin_a_suivre:
+            return self.chemin_a_suivre.pop(0) #renvoie le premier élément de la liste et le supprime pour la prochaine étape 
+        
+        # Sinon, on calcule un nouveau chemin BFS
+        
+        
+        chemin = self._bfs(fourmi, labyrinthe, sim)
+        if chemin is None or len(chemin) == 0:
+            # Pas de chemin trouvé, on reste immobile (ou on peut revenir à Exploration)
+            return None
+        
+        # Sauvegarder le chemin sauf la première étape (car on est déjà à start)
+        self.chemin_a_suivre = chemin[1:]
+        if self.chemin_a_suivre:
+            return self.chemin_a_suivre.pop(0)
+        else:
+            return None
+
+    def choisir_pheromone(self, fourmi, labyrinthe):
+        # On peut déposer des phéromones attractives sur le chemin suivi
+        return "attractif"
+
+    def _bfs(self, fourmi, labyrinthe, sim):
+        """
+        BFS pour trouver le chemin le plus court du start au goal.
+        Retourne la liste des directions (str) à suivre, ou None si pas de chemin.
+        """
+        start = fourmi.position
+        goal = sim.puit_nourriture
+        # Queue pour BFS : stocke les tuples (position, chemin pour y arriver)
+        queue = deque() #c'est simplement une liste vide, mais version optimisé par python pour les .append() et .pop()
+        queue.append( (start, []) )
+
+        # Ensemble des positions visitées
+        visited = set()  #Liste ne pouvant pas contenir 2 fois le même élément
+        visited.add(start)
+
+        while queue:
+            position, path = queue.popleft()
+            if position == goal:
+                return path
+            
+            
+            # On récupère les directions possibles 
+            fourmi.position = position  #les téleportations sont autorisé ici car on est en recherche de chemin, et non dans l'étape "se_deplacer" de la simulation
+            directions = fourmi.chemins_possible(labyrinthe)
+            
+            # Pour chaque direction possible, on calcule la nouvelle position
+            for direction in directions:
+                ni, nj = fourmi.conversion_str_int(direction)
+                
+                # Vérifier que la case est non visitée
+                if (ni, nj) not in visited :
+                    visited.add((ni, nj))
+                    queue.append( ((ni, nj), path + [direction]) )
+        
+        # Aucun chemin trouvé
+        return None
+
+
+if __name__ == "__main__" :
+    lab = Labyrinthe(12, 7)
+    lab.creation_labyrinthe()
+   
+
+
+    sim = Simulation(lab, 20, 60)
+    pos_fourmilliere = sim.position_fourmiliere
+    pos_nourriture = sim.puit_nourriture
+
+    lab.afficher_labyrinthe()
+    lab.afficher_fourmilliere(pos_fourmilliere)
+    lab.afficher_nourriture(pos_nourriture)
+    f1 = Fourmi(pos_fourmilliere, Super())
+    path_to_nourriture = f1.comportement._bfs(f1, lab, sim)
+    print(path_to_nourriture)
+
     plt.show()
-
-
-
-
+    
